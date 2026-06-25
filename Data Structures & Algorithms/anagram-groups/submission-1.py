c8add3fdd05b2cb303caf81454c29fa0a9438bbb
Jack <98520159+JackMatanky@users.noreply.github.com>
2026-06-25 20:14:56 +0300

    Update: anagram-groups - submission-1

--- Data Structures & Algorithms/anagram-groups/submission-1.py
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -1,12 +1,7 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        registry: dict[str, list[str]] = {}
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        registry: dict[str, list[str]] = defaultdict(list)
         for word in strs:
             key: str = "".join(sorted(word))
-            if key not in registry:
-                registry[key] = []
-            
             registry[key].append(word)
-
         return list(registry.values())
-        
