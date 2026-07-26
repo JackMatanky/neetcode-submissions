cfa4f98971a952d5dc642f87f3c74a696ea98728
Jack <98520159+JackMatanky@users.noreply.github.com>
2026-07-26 11:59:30 +0300

    Update: two-integer-sum-ii - submission-0

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -3,9 +3,11 @@ class Solution:
         l, r = 0, len(numbers) - 1
 
         while l < r:
-            if numbers[l] + numbers[r] == target:
+            total = numbers[l] + numbers[r]
+
+            if total == target:
                 return [l + 1, r + 1]
-            if numbers[l] + numbers[r] > target:
+            elif total > target:
                 r -= 1
             else:
                 l += 1
